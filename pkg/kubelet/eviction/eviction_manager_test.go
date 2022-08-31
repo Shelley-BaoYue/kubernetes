2e13d14bd7798be3bf33596a2aec6bf88048993a
Shelley-BaoYue <baoyue2@huawei.com>
2022-08-31 20:48:17 +0800

    revert mirror pod
    Signed-off-by: Shelley-BaoYue <baoyue2@huawei.com>

--- pkg/kubelet/eviction/eviction_manager_test.go
+++ pkg/kubelet/eviction/eviction_manager_test.go
@@ -1253,6 +1253,11 @@ func TestStaticCriticalPodsAreNotEvicted(t *testing.T) {
 	activePodsFunc := func() []*v1.Pod {
 		return pods
 	}
+	mirrorPodFunc := func(staticPod *v1.Pod) (*v1.Pod, bool) {
+		mirrorPod := staticPod.DeepCopy()
+		mirrorPod.Annotations[kubelettypes.ConfigSourceAnnotationKey] = kubelettypes.ApiserverSource
+		return mirrorPod, true
+	}
 
 	fakeClock := clock.NewFakeClock(time.Now())
 	podKiller := &mockPodKiller{}
@@ -1287,6 +1292,7 @@ func TestStaticCriticalPodsAreNotEvicted(t *testing.T) {
 	manager := &managerImpl{
 		clock:                        fakeClock,
 		killPodFunc:                  podKiller.killPodNow,
+		mirrorPodFunc:                mirrorPodFunc,
 		imageGC:                      diskGC,
 		containerGC:                  diskGC,
 		config:                       config,
